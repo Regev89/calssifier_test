247e9d5fabd0605de2da23f35addb55fe2520071
Regev Ace <regevbills@gmail.com>
2024-02-25 09:59:53 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
 import gradio as gr
+from fastai.vision.all import *
 
 
-def greet(name):
-    return "Hello " + name + "!!"
+def classify_img(img):
+    pred, idx, probs = model.predict(img)
+    return dict(zip(model.dls.vocab, probs))
 
 
-iface = gr.Interface(fn=greet, inputs="text", outputs="text")
+image = gr.Image()
+label = gr.Label()
+examples = ['examples/grizzly.jpg', 'examples/black.jpg', 'examples/teddy.jpg']
+
+model = load_learner('export.pkl')
+
+intf = gr.Interface(fn=lambda image: "grizzly bear",
+                    inputs=image, outputs=label, examples=examples)
+iface = gr.Interface(fn=classify_img, inputs=image,
+                     outputs=label, examples=examples)
 iface.launch()
